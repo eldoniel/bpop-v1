063517758f8ffb7cd2a3d0bddf0bed5af72d9f7d
eldoniel <nicofourdu@hotmail.fr>
2017-08-16 10:10:11 +0200

    Implement security w/ UserBundle

---
+++ src/UserBundle/Controller/SecurityController.php
@@ -0,0 +1,30 @@
+<?php
+namespace UserBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+class SecurityController extends Controller
+{
+  /**
+  * @Route("/login", name="login")
+  */
+  public function loginAction(Request $request)
+  {
+    // Si le visiteur est déjà identifié, on le redirige vers l'accueil
+    if ($this->get('security.authorization_checker')->isGranted('IS_AUTHENTICATED_REMEMBERED')) {
+      return $this->redirectToRoute('news_index');
+    }
+
+    // Le service authentication_utils permet de récupérer le nom d'utilisateur
+    // et l'erreur dans le cas où le formulaire a déjà été soumis mais était invalide
+    // (mauvais mot de passe par exemple)
+    $authenticationUtils = $this->get('security.authentication_utils');
+
+    return $this->render('UserBundle:Security:login.html.twig', array(
+      'last_username' => $authenticationUtils->getLastUsername(),
+      'error'         => $authenticationUtils->getLastAuthenticationError(),
+    ));
+  }
+}
